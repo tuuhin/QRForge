60d3f693bc1f3b4513853c7d11661334af13cc9d
tuuhin <tuhinbhowmick2513@gmail.com>
2025-12-05 20:34:32 +0530

    Updated the version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
 		applicationId = "com.sam.qrforge"
 		minSdk = 26
 		targetSdk = 36
-		versionCode = 7
-		versionName = "1.0.1"
+		versionCode = 8
+		versionName = "1.0.2"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
